b74bdbfe609004c0022531ddf0f70791f5adb8d9
Aakash Makhija <aakashmakhija19@gmail.com>
2022-08-09 16:28:30 +0200

    Unit Setup:  Added unit rotation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -18,6 +18,8 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
 
             unitAnimator.SetBool("IsWalking", true);
         }

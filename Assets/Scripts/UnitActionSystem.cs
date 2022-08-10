1182aa7392dec08e2f2043aaa4a8147eff8a794a
Aakash Makhija <aakashmakhija19@gmail.com>
2022-08-10 10:23:17 +0200

    Unit Setup: Added unit selection

---
+++ Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    [SerializeField] LayerMask unitLayerMask;
+    [SerializeField] Unit selectedUnit;
+
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(TryHandleUnitSelection()) return;
+            selectedUnit.Move(MouseWorld.GetPosition());
+        }
+    }
+
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                selectedUnit = unit;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+}

1a1190d7258ca2288fe9bffc394de2a4e61bf7b1
Aakash Makhija <aakashmakhija19@gmail.com>
2022-08-11 10:46:00 +0200

    Grid Setup: grid object created

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class TestingScript : MonoBehaviour
 {
+    [SerializeField] Transform gridObjectPrefab;
+
     private GridSystem gridSystem;
 
     private void Start()
     {
         gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridObjectPrefab);
     }
 
     private void Update()

d390bc036975a40a4a19258307244240f90bdfd7
Aakash Makhija <aakashmakhija19@gmail.com>
2022-08-30 13:20:22 +0200

    Unit Ragdoll implemented

---
+++ Assets/Scripts/UnitRagdoll.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitRagdoll : MonoBehaviour
+{
+    [SerializeField] Transform ragdollRootBone;
+    [SerializeField] float TestexplosionForce = 300f;
+    [SerializeField] float TestexplosionRange = 10f;
+
+
+    public void Setup(Transform originalRootbone)
+    {
+        MatchAllChildTransforms(originalRootbone, ragdollRootBone);
+        ApplyExplosionToRagdoll(ragdollRootBone, TestexplosionForce, transform.position, TestexplosionRange);
+    }
+
+    private void MatchAllChildTransforms(Transform root, Transform clone)
+    {
+        foreach (Transform child in root)
+        {
+            Transform cloneChild = clone.Find(child.name);
+            if (cloneChild != null)
+            {
+                cloneChild.position = child.position;
+                cloneChild.rotation = child.rotation;
+
+                //Recursive call for child of child bones
+                MatchAllChildTransforms(child, cloneChild);
+            }
+        }
+    }
+
+    private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach(Transform child in root)
+        {
+            if(child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, explosionRange);
+
+        }
+    }
+
+
+}

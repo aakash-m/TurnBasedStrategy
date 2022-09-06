03a1207a24dfea11484b7fab5877f47ae30b8ecc
Aakash Makhija <aakashmakhija19@gmail.com>
2022-09-06 11:24:25 +0200

    Added pathfinding script

--- Assets/Scripts/LevelGrid.cs
+++ Assets/Scripts/LevelGrid.cs
@@ -25,7 +25,7 @@ public class LevelGrid : MonoBehaviour
         Instance = this;
 
         gridSystem = new GridSystem<GridObject>(10, 10, 2f, (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
-        gridSystem.CreateDebugObjects(gridObjectPrefab);
+        //gridSystem.CreateDebugObjects(gridObjectPrefab);
     }
 
     public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit)
